7c1045ebe7fc2321c7a829856dbde133bc5e1d27
Zakhar_Savchuk <savchukzahar33@gmail.com>
2020-04-09 12:35:46 +0300

    SPA part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Menu from './MenuComponent';
+import DishDetail from './DishdetailComponent';
 import { DISHES } from '../shared/dishes';
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -34,20 +35,30 @@ class Main extends Component {
                 />
             );
         }
+
+        const DishWithId = ({match}) =>{
+            return(
+                <DishDetail dish={this.state.dishes.filter((dish)=> dish.id === parseInt(match.params.dishId,10))[0]}
+                comments = {this.state.comments.filter((comment)=> comment.dishId === parseInt(match.params.dishId,10))}
+                />
+            );
+        }
+
         return (
             <div>
                 <Header />
                 <Switch>
                     <Route path='/home' component={HomePage} />
                     <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+                    <Route path='/menu/:dishId' component={DishWithId} />
                     <Route exact path='/contactus' component={Contact} />}/>
                    <Redirect to="/home" />
                 </Switch>
                 <Footer />
             </div>
         );
+        }
+
     }
 
-}
-
-export default Main;
+    export default Main;
